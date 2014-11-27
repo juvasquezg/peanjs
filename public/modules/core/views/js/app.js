43d8be9cf90d90d7f21c8b7f3f4be04ebb25a549
“juvasquezg” <juvasquez88@gmail.com>
2014-11-27 14:53:25 -0500

    Postgresql, Express, Angular, Node Js

---
+++ public/modules/core/views/js/app.js
@@ -0,0 +1,27 @@
+(function () {
+  'use string';
+  var app = angular.module('pokedex', []);
+
+  app.controller('PokemonController', function () {
+    this.pokemon = {
+      id: "001",
+      name: "Bulbasaur",
+      species: "Seed Pokémon",
+      type: [ "Grass", "Poison" ],
+      height: "2′4″ (0.71m)",
+      weight: "15.2 lbs (6.9 kg)",
+      abilities: [ "Overgrow", "Chlorophyll" ],
+      stats: {
+        hp: 45,
+        attack: 49,
+        defense: 49,
+        "sp.atk": 65,
+        "sp.def": 65,
+        speed: 45,
+        total: 318
+      },
+      evolution: [ "Bulbasaur", "Ivysaur", "Venusaur" ]
+    };
+  });
+
+})();
